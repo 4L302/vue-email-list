e860a0811b983ebf73da78da77b26de2ef40f8e5
4L302 <alegarg2002@gmail.com>
2023-04-11 16:50:20 +0200

    .

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            titolo: 'Generatore email',
+            basePath: 'https://flynn.boolean.careers/exercises/api/random/mail'
+        }
+    },
+    methods: {
+
+    },
+    mounted(){
+
+    },
+}).mount('#app');
